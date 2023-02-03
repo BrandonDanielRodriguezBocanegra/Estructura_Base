18c305202b22e184c5420eadc5cf11065f76afc1
BrandonDanielRodriguezBocanegra <adustgore5@gmail.com>
2023-02-03 16:26:10 -0600

    Se agrega el proyecto

---
+++ 02_Hola_Mundo.php
@@ -0,0 +1,23 @@
+<?php
+
+//variable
+$nombre="Brandon";
+
+echo "Hola mundo mi nombre es" .$nombre;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina web Brandon</title>
+</head>
+<body>
+    <h1>Pagina web de Brandon</h1>
+    <h1><?php echo 'hola'. $nombre ?></h1>
+    
+</body>
+</html>
+
